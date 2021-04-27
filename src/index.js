2d4c0046b14d93e5d89f10ecb21e276935dc8f70
h11g <huangweiqiang@codemao.cn>
2021-04-27 20:40:49 +0800

    feat: 懒加载

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
-import _ from 'lodash'
+// import _ from 'lodash'
 import { cube } from './math.js'
 import './style.css'
 
-function component () {
-  const element = document.createElement('pre')
+function getComponent () {
+  return import('lodash').then(({ default: _ }) => {
+    const element = document.createElement('div')
+    element.innerHTML = _.join([
+      'Hello webpack!',
+      '5 cubed is equal to ' + cube(5)
+    ], ' ')
 
-  element.innerHTML = _.join([
-    'Hello webpack!',
-    '5 cubed is equal to ' + cube(5)
-  ])
-
-  return element
+    return element
+  })
 }
 
-document.body.appendChild(component())
+// function component () {
+//   const element = document.createElement('pre')
+
+//   element.innerHTML = _.join([
+//     'Hello webpack!',
+//     '5 cubed is equal to ' + cube(5)
+//   ])
+
+//   return element
+// }
+
+// document.body.appendChild(component())
+getComponent().then((component) => {
+  document.body.appendChild(component)
+})
