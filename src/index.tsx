e2a29ad37758a41647b0c504bb1f333da37d2b10
h11g <huangweiqiang@codemao.cn>
2021-05-02 21:02:42 +0800

    feat: some

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'src/app'
 
-if (module && module.hot) {
-  console.log('hot==>')
-  module.hot.accept()
-}
-
 ReactDOM.render(<App name='vortesnail' age={25} />, document.querySelector('#root'))
